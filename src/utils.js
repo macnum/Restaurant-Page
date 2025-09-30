6f2b74718315d2ced5eb97e3b99a2392bddcd61b
macnum <w.damilola@yahoo.com>
2025-09-30 20:43:03 +0100

    feat: add utility function

---
+++ src/utils.js
@@ -0,0 +1,40 @@
+export function createDiv(className) {
+  const div = document.createElement("div");
+  div.classList.add(className);
+  return div;
+}
+
+export function createText(text) {
+  const textNode = document.createTextNode(text);
+  return textNode;
+}
+
+export function createParagraph(textNode) {
+  const paragraph = document.createElement("p");
+  paragraph.appendChild(textNode);
+  return paragraph;
+}
+
+export function createSection(className) {
+  const section = document.createElement("section");
+  section.classList.add(className);
+  return section;
+}
+export function createUnorderedList(items) {
+  const list = document.createElement("ul");
+  items.forEach((item) => {
+    const listItem = createListItem(item);
+    list.appendChild(listItem);
+  });
+  return list;
+}
+export function createListItem(item) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = item;
+  return listItem;
+}
+export function createHeading(level, text) {
+  const heading = document.createElement(`h${level}`);
+  heading.textContent = text;
+  return heading;
+}
